b621cf25aa6fa6a30914b3438b3f8c1018d35e44
Kay Kaprolat <Frank_Kay.Kaprolat@mailbox.tu-dresden.de>
2021-12-08 16:58:59 +0100

    first draft for database hardening

---
+++ docker/database_hardening.sh
@@ -0,0 +1,17 @@
+# root pw is set by the docker image / env variables, so no need to change it here
+
+# enable unix_socket authentication
+/usr/bin/mariadb < "UPDATE mysql.global_priv SET priv=json_set(priv, '\$.password_last_changed', UNIX_TIMESTAMP(), '\$.plugin', 'mysql_native_password', '\$.authentication_string', 'invalid', '\$.auth_or', json_array(json_object(), json_object('plugin', 'unix_socket'))) WHERE User='root';"
+
+# remove anonymous users
+/usr/bin/mariadb < "DELETE FROM mysql.global_priv WHERE User='';"
+
+# remove remote root
+/usr/bin/mariadb < "DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+
+# remove test db and access to it
+/usr/bin/mariadb < "DROP DATABASE IF EXISTS test;"
+/usr/bin/mariadb < "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
+
+# reload privileges
+/usr/bin/mariadb < "FLUSH PRIVILEGES;"
