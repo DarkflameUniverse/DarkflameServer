f772fedf19b9612b2610770136a0418b9a13b0fa
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2021-12-22 00:43:42 -0800

    Updated description of SetCoins
    Updated the description of the Character::SetCoins function to more accurately describe how the function works.

--- dGame/Character.h
+++ dGame/Character.h
@@ -309,8 +309,7 @@ public:
 	const int64_t GetCoins() const { return m_Coins; }
 
     /**
-     * Updates the current amount of coins of the character by a specified amount, for achievements this is not sent
-     * as it's tracked by the client
+     * Updates the current amount of coins of the character by a specified amount
      * @param newCoins the amount of coins to update by
      * @param coinSource The source of the loot
      */
