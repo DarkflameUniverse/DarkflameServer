db41cd88108071d7cffe3912a02d4a330f13725a
Verathian <verathian@gmail.com>
2021-12-23 01:28:22 -0500

    Added NsDanceNpcs.cpp and .h for mission 595 (Boogie Down Achievement)

---
+++ dScripts/NsDanceNpcs.cpp
@@ -0,0 +1,30 @@
+﻿#include "NsDanceNpcs.h"
+#include "GameMessages.h"
+#include "MissionComponent.h"
+
+void NsDanceNpcs::OnStartup(Entity* self) 
+{
+}
+
+void NsDanceNpcs::OnEmoteReceived(Entity* self, const int32_t emote, Entity* target)
+{
+	int validEmotes[] = { 383, 359, 377, 209, 208, 379, 380, 207, 382, 378, 381 };
+
+	if (!std::find(std::begin(validEmotes), std::end(validEmotes), emote))
+	{
+		return;
+	}
+
+	auto* missionComponent = target->GetComponent<MissionComponent>();
+	
+	if (missionComponent != nullptr)
+	{
+		if (self->GetLOT() == 5006) // taskID(uid) = 898 for 5006 (Kant Dance)
+			missionComponent->ForceProgress(595, 898, 1, false);
+		else if (self->GetLOT() == 5007) // taskID(uid) = 899 for 5007 (Sofie Cushion)
+			missionComponent->ForceProgress(595, 899, 1, false);
+		else if (self->GetLOT() == 3002) // taskID(uid) = 900 for 3002 (Peppy Slapbiscuit)
+			missionComponent->ForceProgress(595, 900, 1, false);
+	}
+}
+
---
+++ dScripts/NsDanceNpcs.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "CppScripts.h"
+
+class NsDanceNpcs final : public CppScripts::Script
+{
+public:
+	void OnStartup(Entity* self) override;
+	void OnEmoteReceived(Entity* self, int32_t emote, Entity* target) override;
+};
