d580f2d7dc586203695e6d9f0b72d7f973d7ec9d
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-01-07 02:45:46 -0800

    Changed how max level coins are sent
    Added a variable that, if the entity completing the mission is the max level, will set the variable to that value and then send that with the currency reward that YieldRewards() would normally call.

--- dGame/dMission/Mission.cpp
+++ dGame/dMission/Mission.cpp
@@ -423,11 +423,12 @@ void Mission::YieldRewards() {
         }
     }
 
+    __int64 coinsToSend = 0;
     if (info->LegoScore > 0) {
         eLootSourceType lootSource = info->isMission ? LOOT_SOURCE_MISSION : LOOT_SOURCE_ACHIEVEMENT;
-        if(characterComponent->GetLevel() >= characterComponent->GetMaxLevel()) {
+        if(characterComponent->GetLevel() >= dZoneManager::Instance()->GetMaxLevel()) {
             // Since the character is at the level cap we reward them with coins instead of UScore.
-            character->SetCoins(character->GetCoins() + info->LegoScore * m_CurrencyConversionRate, lootSource);
+            coinsToSend += info->LegoScore * dZoneManager::Instance()->GetLevelCapCurrencyConversion();
         } else {
             characterComponent->SetUScore(characterComponent->GetUScore() + info->LegoScore);
             GameMessages::SendModifyLEGOScore(entity, entity->GetSystemAddress(), info->LegoScore, lootSource);
@@ -492,9 +493,9 @@ void Mission::YieldRewards() {
         inventoryComponent->AddItem(pair.first, count);
     }
 
-    if (info->reward_currency > 0) {
+    if (info->reward_currency > 0 || coinsToSend > 0) {
         eLootSourceType lootSource = info->isMission ? LOOT_SOURCE_MISSION : LOOT_SOURCE_ACHIEVEMENT;
-        character->SetCoins(character->GetCoins() + info->reward_currency, lootSource);
+        character->SetCoins(character->GetCoins() + info->reward_currency + coinsToSend, lootSource);
     }
 
     if (info->reward_maxinventory > 0) {
