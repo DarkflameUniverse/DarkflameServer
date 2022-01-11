24024eda690b122e7a3833c344ca093cb9dff779
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-01-10 23:05:25 -0800

    Cleaned up GrowingFlower.cpp script

--- dScripts/GrowingFlower.cpp
+++ dScripts/GrowingFlower.cpp
@@ -7,22 +7,13 @@ void GrowingFlower::OnSkillEventFired(Entity *self, Entity *target, const std::s
         self->SetNetworkVar(u"blooming", true);
         self->AddTimer("FlowerDie", GrowingFlower::aliveTime);
 
-        const auto mission1 = self->GetVar<int32_t>(u"missionID");
-        const auto mission2 = self->GetVar<int32_t>(u"missionID2");
-
-        LootGenerator::Instance().DropActivityLoot(target, self, self->GetLOT(), 0);
+        LootGenerator::Instance().DropActivityLoot(target, self, self->GetLOT());
 
         auto* missionComponent = target->GetComponent<MissionComponent>();
-        if (missionComponent != nullptr) {
-            for (const auto mission : achievementIDs)
-                missionComponent->ForceProgressTaskType(mission, static_cast<uint32_t>(MissionTaskType::MISSION_TASK_TYPE_SCRIPT), 1);
 
-            if (mission1 && missionComponent->GetMissionState(mission1) == MissionState::MISSION_STATE_ACTIVE)
-                missionComponent->ForceProgressTaskType(mission1, static_cast<uint32_t>(MissionTaskType::MISSION_TASK_TYPE_SCRIPT), 1);
-
-            if (mission2 && missionComponent->GetMissionState(mission2) == MissionState::MISSION_STATE_ACTIVE)
-                missionComponent->ForceProgressTaskType(mission2, static_cast<uint32_t>(MissionTaskType::MISSION_TASK_TYPE_SCRIPT), 1);
-        }
+        if (missionComponent == nullptr) return;
+        
+        missionComponent->Progress(MissionTaskType::MISSION_TASK_TYPE_SCRIPT, self->GetLOT());
     }
 }
 
@@ -31,5 +22,3 @@ void GrowingFlower::OnTimerDone(Entity *self, std::string message) {
         self->Smash();
     }
 }
-
-const std::vector<uint32_t> GrowingFlower::achievementIDs = { 143, 152, 153, 1409, 1507, 1544, 1581, 1845 };
