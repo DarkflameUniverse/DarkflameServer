cd07295b071de429c7e4498da3c42813ac3bb854
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-01-11 04:23:12 -0800

    Cleaned up DieRoll and implemented modular code

--- dScripts/LegoDieRoll.cpp
+++ dScripts/LegoDieRoll.cpp
@@ -13,43 +13,14 @@ void LegoDieRoll::OnTimerDone(Entity* self, std::string timerName) {
         self->Smash(self->GetObjectID(), SILENT);
     } 
     else if (timerName == "ThrowDice") {
-        int dieRoll = GeneralUtils::GenerateRandomNumber<int>(1, 6);
+        int32_t dieRoll = GeneralUtils::GenerateRandomNumber<int32_t>(1, 6);
+        GameMessages::SendPlayAnimation(self, anims[dieRoll - 1]);
+        if (dieRoll == 6) {
+            auto* user = self->GetOwner();
+            auto* missionComponent = user->GetComponent<MissionComponent>();
+            if(missionComponent == nullptr) return;
 
-        switch (dieRoll)
-        {
-        case 1:
-		    GameMessages::SendPlayAnimation(self, u"roll-die-1");
-            break;
-        case 2:
-		    GameMessages::SendPlayAnimation(self, u"roll-die-2");
-            break;
-        case 3:
-		    GameMessages::SendPlayAnimation(self, u"roll-die-3");
-            break;
-        case 4:
-		    GameMessages::SendPlayAnimation(self, u"roll-die-4");
-            break;
-        case 5:
-		    GameMessages::SendPlayAnimation(self, u"roll-die-5");
-            break;
-        case 6:
-            {
-		    GameMessages::SendPlayAnimation(self, u"roll-die-6");
-            // tracking the It's Truly Random Achievement 
-            auto* owner = self->GetOwner();
-            auto* missionComponent = owner->GetComponent<MissionComponent>();
-
-            if (missionComponent != nullptr) {
-                const auto rollMissionState = missionComponent->GetMissionState(756);
-                if (rollMissionState == MissionState::MISSION_STATE_ACTIVE) {
-                    missionComponent->ForceProgress(756, 1103, 1);
-                }
-            }
-            break;
-            }
-        default:
-	        Game::logger->LogDebug("LegoDieRoll", "Invalid animation: roll-die-%i\n", dieRoll);
-            break;
+            missionComponent->Progress(MissionTaskType::MISSION_TASK_TYPE_SCRIPT, self->GetLOT());
         }
     }
 }
--- dScripts/LegoDieRoll.h
+++ dScripts/LegoDieRoll.h
@@ -7,5 +7,6 @@ public:
 	void OnTimerDone(Entity* self, std::string timerName);
 private:
     constexpr static const float animTime = 2.0f;
+	const std::u16string anims[6] = { u"roll-die-1", u"roll-die-2", u"roll-die-3", u"roll-die-4", u"roll-die-5", u"roll-die-6"};
 };
 
