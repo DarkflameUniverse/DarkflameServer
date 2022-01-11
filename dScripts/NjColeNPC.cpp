834a13ad909f2b6c8eb75fd214c5d36185a8c483
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-01-11 15:25:15 -0800

    Cleaned up NjColeNPC

--- dScripts/NjColeNPC.cpp
+++ dScripts/NjColeNPC.cpp
@@ -4,56 +4,39 @@
 
 void NjColeNPC::OnEmoteReceived(Entity* self, int32_t emote, Entity* target) 
 {
-    if (emote != 393)
-    {
-        return;
-    }
+    // Check if we used the "Dragon Roar" emote.
+    if (emote != 393) return;
 
     auto* inventoryComponent = target->GetComponent<InventoryComponent>();
 
-    if (inventoryComponent == nullptr)
-    {
-        return;
-    }
+    if (inventoryComponent == nullptr) return;
 
-    if (!inventoryComponent->IsEquipped(14499) && !inventoryComponent->IsEquipped(16644))
-    {
-        return;
-    }
+    // Make sure we have a "Dragon Mask" equipped.
+    if (!inventoryComponent->IsEquipped(14499) && !inventoryComponent->IsEquipped(16644)) return;
 
     auto* missionComponent = target->GetComponent<MissionComponent>();
 
-    if (missionComponent == nullptr)
-    {
-        return;
-    }
+    if (missionComponent == nullptr) return;
 
-    missionComponent->ForceProgressTaskType(1818, 1, 1);
+    missionComponent->Progress(MissionTaskType::MISSION_TASK_TYPE_SCRIPT, self->GetLOT());
 }
 
 void NjColeNPC::OnMissionDialogueOK(Entity* self, Entity* target, int missionID, MissionState missionState) 
 {
     NjNPCMissionSpinjitzuServer::OnMissionDialogueOK(self, target, missionID, missionState);
 
-    if (missionID == 1818 && missionState >= MissionState::MISSION_STATE_READY_TO_COMPLETE)
-    {
-        auto* missionComponent = target->GetComponent<MissionComponent>();
-        auto* inventoryComponent = target->GetComponent<InventoryComponent>();
+    auto* missionComponent = target->GetComponent<MissionComponent>();
 
-        if (missionComponent == nullptr || inventoryComponent == nullptr)
-        {
-            return;
-        }
+    if (missionComponent == nullptr) return;
 
-        if (inventoryComponent->GetLotCount(14499) > 0)
-        {
-            inventoryComponent->RemoveItem(14499, 1);
-        }
-        else
-        {
-            return;
-        }
+    if (missionComponent->GetMissionState(missionID) != MissionState::MISSION_STATE_READY_TO_COMPLETE) return;
+    
+    auto* inventoryComponent = target->GetComponent<InventoryComponent>();
 
-        inventoryComponent->AddItem(16644, 1);
-    }
+    if (inventoryComponent == nullptr) return;
+
+    if (inventoryComponent->GetLotCount(14499) == 0) return;
+
+    inventoryComponent->RemoveItem(14499, 1);
+    inventoryComponent->AddItem(16644, 1);
 }
