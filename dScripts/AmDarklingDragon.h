67d5061416a6b253a11506428115e1e7ffac5a2a
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-01-19 02:10:04 -0800

    Implemented Dragon Smash Quick Build for Crux Prime Dragons
    Implemented a script for the crux prime dragons to spawn the Golem Quick Build that insta kills the Entity if the player hits the dragon after the golem is built.  Tested on Local Ubuntu instance against 1 Crux Prime Maelstrom Dragon and 1 Butterscorch and had no issues building the quick build or with the enemy not smashing.  Enemies also correctly reset to their default behavior after the stun period has expired.

---
+++ dScripts/AmDarklingDragon.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "CppScripts.h"
+
+class AmDarklingDragon : public CppScripts::Script
+{
+public:
+    void OnStartup(Entity* self) override;
+    void OnDie(Entity* self, Entity* killer) override;
+    void OnHitOrHealResult(Entity* self, Entity* attacker, int32_t damage) override;
+    void OnTimerDone(Entity* self, std::string timerName) override;
+    void OnFireEventServerSide(Entity *self, Entity *sender, std::string args, int32_t param1, int32_t param2, int32_t param3) override;
+};
