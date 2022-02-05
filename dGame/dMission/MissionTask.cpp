933cdee414e6abad4df40186c4ce43748e64f55e
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2022-02-05 04:08:40 -0800

    Implemented Model Pickup and Reputation achievements (#413)
    * Implemented Model Pickup and Reputation achievements
    
    * Moved mission progression to placement
    
    * Changed name to place

--- dGame/dMission/MissionTask.cpp
+++ dGame/dMission/MissionTask.cpp
@@ -460,6 +460,7 @@ void MissionTask::Progress(int32_t value, LWOOBJID associate, const std::string&
 	case MissionTaskType::MISSION_TASK_TYPE_SMASH:
 	case MissionTaskType::MISSION_TASK_TYPE_ITEM_COLLECTION:
 	case MissionTaskType::MISSION_TASK_TYPE_PLAYER_FLAG:
+	case MissionTaskType::MISSION_TASK_TYPE_EARN_REPUTATION:
 	{
 		if (!InAllTargets(value)) break;
 
@@ -467,7 +468,11 @@ void MissionTask::Progress(int32_t value, LWOOBJID associate, const std::string&
 
 		break;
 	}
-		
+	case MissionTaskType::MISSION_TASK_TYPE_PLACE_MODEL:
+	{
+		AddProgress(count);
+		break;
+	}
 	default:
 		Game::logger->Log("MissionTask", "Invalid mission task type (%i)!\n", static_cast<int>(type));
 		return;
--- dGame/dMission/MissionTaskType.h
+++ dGame/dMission/MissionTaskType.h
@@ -16,9 +16,11 @@ enum class MissionTaskType : int {
     MISSION_TASK_TYPE_MINIGAME = 14,                    //!< A task for doing something in a minigame
     MISSION_TASK_TYPE_NON_MISSION_INTERACTION = 15,     //!< A task for interacting with a non-mission
     MISSION_TASK_TYPE_MISSION_COMPLETE = 16,            //!< A task for completing a mission
+    MISSION_TASK_TYPE_EARN_REPUTATION = 17,             //!< A task for earning reputation
     MISSION_TASK_TYPE_POWERUP = 21,                     //!< A task for collecting a powerup
     MISSION_TASK_TYPE_PET_TAMING = 22,                  //!< A task for taming a pet
     MISSION_TASK_TYPE_RACING = 23,                      //!< A task for racing
     MISSION_TASK_TYPE_PLAYER_FLAG = 24,                 //!< A task for setting a player flag
+    MISSION_TASK_TYPE_PLACE_MODEL = 25,                //!< A task for picking up a model
     MISSION_TASK_TYPE_VISIT_PROPERTY = 30               //!< A task for visiting a property
 };
