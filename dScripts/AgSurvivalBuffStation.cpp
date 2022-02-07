f41e8292e82297c07b07dae74466539d97d571bf
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-02-06 21:34:26 -0800

    Clarified numbers and added comments

--- dScripts/AgSurvivalBuffStation.cpp
+++ dScripts/AgSurvivalBuffStation.cpp
@@ -5,14 +5,14 @@
 
 void AgSurvivalBuffStation::OnRebuildComplete(Entity* self, Entity* target) {
     auto destroyableComponent = self->GetComponent<DestroyableComponent>();
-    
+    // We set the faction to 6 so that the buff station sees players as friendly targets
     if (destroyableComponent != nullptr) destroyableComponent->SetFaction(6);
 
     auto skillComponent = self->GetComponent<SkillComponent>();
 
     if (skillComponent == nullptr) return;
 
-    skillComponent->CalculateBehavior(201, 1784, self->GetObjectID());
+    skillComponent->CalculateBehavior(skillIdForBuffStation, behaviorIdForBuffStation, self->GetObjectID());
 
     self->AddCallbackTimer(10.0f, [self]() {
         self->Smash();
