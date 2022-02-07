3de2c3bfc63a44672c3ba3ea9eb29cd019e4bb40
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-02-06 21:32:36 -0800

    set faction of buff station to 6

--- dScripts/AgSurvivalBuffStation.cpp
+++ dScripts/AgSurvivalBuffStation.cpp
@@ -1,8 +1,13 @@
 #include "AgSurvivalBuffStation.h"
 #include "SkillComponent.h"
 #include "dLogger.h"
+#include "DestroyableComponent.h"
 
 void AgSurvivalBuffStation::OnRebuildComplete(Entity* self, Entity* target) {
+    auto destroyableComponent = self->GetComponent<DestroyableComponent>();
+    
+    if (destroyableComponent != nullptr) destroyableComponent->SetFaction(6);
+
     auto skillComponent = self->GetComponent<SkillComponent>();
 
     if (skillComponent == nullptr) return;
