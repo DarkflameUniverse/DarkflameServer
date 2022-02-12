c0bbd84c146506500ffd23a8a29eaade925c8ad2
Nils Bergmann <nilsbergmann@noim.io>
2022-02-12 15:04:55 +0100

    Put waiting for setup in a separate script

--- docker/start_server.sh
+++ docker/start_server.sh
@@ -32,16 +32,7 @@ function symlink_config_files() {
     ln -s /shared_configs/configs/worldconfig.ini /app/worldconfig.ini
 }
 
-# check to make sure the setup has completed
-while [ ! -f "/shared_configs/extracted" ] || [ ! -f "/shared_configs/migrated" ] || [ ! -f "/shared_configs/root_dir" ]; do
-    echo "Client setup not finished. Waiting for setup container to complete..."
-    sleep 5
-    if [ -f /shared_configs/error ]; then
-        echo "[ERROR] It looks like the setup container exited with an error"
-        echo "[ERROR] Look at the logs of the setup container to find out what happened"
-        exit 1
-    fi
-done
+./wait-for-setup.sh
 
 CLIENT_ROOT=`cat /shared_configs/root_dir`
 
@@ -54,10 +45,6 @@ if [[ ! -f "/app/initialized" ]]; then
 else
     echo "Server already initialized"
 fi
-
-echo "Wait for database to be ready"
-
-/wait
 
 # start the server
 echo "Starting MasterServer"
