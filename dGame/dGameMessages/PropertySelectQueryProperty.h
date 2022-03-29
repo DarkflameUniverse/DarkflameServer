69ec1216dc999564df7bbc027eb8186cf64b1dd8
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-03-28 20:51:19 -0700

    added comments

--- dGame/dGameMessages/PropertySelectQueryProperty.h
+++ dGame/dGameMessages/PropertySelectQueryProperty.h
@@ -9,17 +9,17 @@ public:
 
 	void Deserialize(RakNet::BitStream& stream) const;
 
-    LWOCLONEID CloneId = LWOCLONEID_INVALID;
-    std::string OwnerName = "";
-    std::string Name = "";
-    std::string Description = "";
-    uint32_t Reputation = 0;
-    bool IsBestFriend = false;
-    bool IsFriend = false;
-    bool IsModeratorApproved = false;
-    bool IsAlt = false;
-    bool IsOwned = false;
-    uint32_t AccessType = 0;
-    uint32_t DateLastPublished = 0;
-    uint64_t PerformanceCost = 0;
+    LWOCLONEID CloneId = LWOCLONEID_INVALID;        // The cloneID of the property
+    std::string OwnerName = "";                     // The property owners name
+    std::string Name = "";                          // The property name
+    std::string Description = "";                   // The property description
+    uint32_t Reputation = 0;                        // The reputation of the property
+    bool IsBestFriend = false;                      // Whether or not the property belongs to a best friend
+    bool IsFriend = false;                          // Whether or not the property belongs to a friend
+    bool IsModeratorApproved = false;               // Whether or not a moderator has approved this property
+    bool IsAlt = false;                             // Whether or not the property is owned by an alt of the account owner
+    bool IsOwned = false;                           // Whether or not the property is owned
+    uint32_t AccessType = 0;                        // The privacy option of the property
+    uint32_t DateLastPublished = 0;                 // The last day the property was published
+    uint64_t PerformanceCost = 0;                   // The performance cost of the property
 };
