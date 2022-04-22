2e61839d93728d4193b5542558369f8cf05be61d
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-04-22 00:10:15 -0700

    isPlayer changes
    Changed isPlayer to check for whether the entity has a character instead of checking system address of entity.

--- dGame/Entity.cpp
+++ dGame/Entity.cpp
@@ -1739,7 +1739,7 @@ void Entity::CancelAllTimers() {
 }
 
 bool Entity::IsPlayer() const {
-	return m_TemplateID == 1 && GetSystemAddress() != UNASSIGNED_SYSTEM_ADDRESS;
+	return m_TemplateID == 1 && GetCharacter() != nullptr;
 }
 
 void Entity::TriggerEvent(std::string eventID, Entity* optionalTarget) {
