763a1f4a613c2de8d438a4f64be4302b5005bcaf
Aaron Kimbre <aronwk.aaron@gmail.com>
2022-05-01 08:54:45 -0500

    Split out Darkitect reveal into it's own file add reveal to Fong

---
+++ dScripts/FvFong.cpp
@@ -0,0 +1,12 @@
+#include "FvFong.h"
+#include "Darkitect.h"
+#include "MissionComponent.h"
+
+void FvFong::OnMissionDialogueOK(Entity* self, Entity* target, int missionID, MissionState missionState)
+{
+	if (missionID == 734 && missionState == MissionState::MISSION_STATE_READY_TO_COMPLETE)
+	{
+		Darkitect Baron;
+		Baron.Reveal(self, target);
+	}
+}
