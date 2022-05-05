88557d2027620a0e9c84f76c8b00a995f054e5bf
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-05-05 02:38:02 -0700

    comments

--- dGame/dGameMessages/AMFDeserialize.h
+++ dGame/dGameMessages/AMFDeserialize.h
@@ -5,6 +5,13 @@
 
 class AMFDeserialize {
     public:
+        /**
+         * Read an AMF3 value from a bitstream.
+         * 
+         * @param inStream inStream to read value from.
+         * @param doFullProcess currently this is here for arrays but should be removed and have its work be done in the AMFArray switch case.
+         * @return Returns an AMFArrayValue with all the information from the bitStream in it.
+         */
         AMFArrayValue* Read(RakNet::BitStream* inStream, bool doFullProcess = false);
     private:
         /**
