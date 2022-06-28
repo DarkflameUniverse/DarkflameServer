7242828b439bfa2452471ff4987aa899eafdd090
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-06-27 20:52:51 -0700

    Add Script
    Add the FireFirstSkillonStartup script to allow for scripts that use this to function.

---
+++ dScripts/FireFirstSkillonStartup.cpp
@@ -0,0 +1,22 @@
+#include "FireFirstSkillonStartup.h"
+#include "Entity.h"
+#include "SkillComponent.h"
+#include "CDClientDatabase.h"
+#include "CDObjectSkillsTable.h"
+
+void FireFirstSkillonStartup::OnStartup(Entity* self) {
+    auto skillComponent = self->GetComponent<SkillComponent>();
+    if (!skillComponent) return;
+
+    CDObjectSkillsTable* skillsTable = CDClientManager::Instance()->GetTable<CDObjectSkillsTable>("ObjectSkills");
+    std::vector<CDObjectSkills> skills = skillsTable->Query([=](CDObjectSkills entry) {return (entry.objectTemplate == self->GetLOT()); });
+
+    for (auto skill : skills) {
+        CDSkillBehaviorTable* skillBehaviorTable = CDClientManager::Instance()->GetTable<CDSkillBehaviorTable>("SkillBehavior");
+		CDSkillBehavior behaviorData = skillBehaviorTable->GetSkillByID(skill.skillID);
+
+        // Should parent entity be null, make the originator self.
+        const auto target = self->GetParentEntity() ? self->GetParentEntity()->GetObjectID() : self->GetObjectID();
+        skillComponent->CalculateBehavior(skill.skillID, behaviorData.behaviorID, LWOOBJID_EMPTY, false, false, target);
+    }
+}
