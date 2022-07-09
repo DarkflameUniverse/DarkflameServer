325dc5a5711fbd5bbff66024820fb392d9e0a599
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2022-07-09 15:57:45 -0700

    Revert "Wait for world to shutdown (#624)" (#626)
    This reverts commit 66edf57b025196b31fc89906f3cc616f827ecb0f.

--- dMasterServer/MasterServer.cpp
+++ dMasterServer/MasterServer.cpp
@@ -303,6 +303,7 @@ int main(int argc, char** argv) {
 
 			if (affirmTimeout == 1000) {
 				instance->Shutdown();
+				instance->SetShutdownComplete(true);
 
 				Game::im->RedirectPendingRequests(instance);
 			}
