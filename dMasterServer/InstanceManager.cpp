e4ef46cd91c893c3e1522d84371ec97589494868
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-07-11 19:54:21 -0700

    Added shutting down check
    Added check for isShuttingDown

--- dMasterServer/InstanceManager.cpp
+++ dMasterServer/InstanceManager.cpp
@@ -290,7 +290,7 @@ Instance * InstanceManager::FindInstance(LWOMAPID mapID, bool isFriendTransfer, 
 
 Instance * InstanceManager::FindInstance(LWOMAPID mapID, LWOINSTANCEID instanceID) {
 	for (Instance* i : m_Instances) {
-		if (i && i->GetMapID() == mapID && i->GetInstanceID() == instanceID && !i->GetIsPrivate()) {
+		if (i && i->GetMapID() == mapID && i->GetInstanceID() == instanceID && !i->GetIsPrivate() && !i->GetShutdownComplete()  && !i->GetIsShuttingDown()) {
 			return i;
 		}
 	}
