701c113976eb77cabbbad59a70ed90c0e44fe389
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-07-15 00:38:17 -0700

    This is good

--- dMasterServer/MasterServer.cpp
+++ dMasterServer/MasterServer.cpp
@@ -358,6 +358,7 @@ void HandlePacket(Packet* packet) {
 		Instance* instance =
 			Game::im->GetInstanceBySysAddr(packet->systemAddress);
 		if (instance) {
+			Game::logger->Log("MasterServer", "Actually disconnected from zone %i clone %i instance %i port %i\n", instance->GetMapID(), instance->GetCloneID(), instance->GetInstanceID(), instance->GetPort());
 			Game::im->RemoveInstance(instance); //Delete the old
 		}
 
@@ -678,8 +679,8 @@ void HandlePacket(Packet* packet) {
 				return;
 			}
 
-			Game::logger->Log("MasterServer", "Got shutdown response\n");
-			instance->SetShutdownComplete(true);
+			Game::logger->Log("MasterServer", "Got shutdown response from zone %i clone %i instance %i port %i\n", instance->GetMapID(), instance->GetCloneID(), instance->GetInstanceID(), instance->GetPort());
+			instance->SetIsShuttingDown(true);
 			break;
 		}
 
