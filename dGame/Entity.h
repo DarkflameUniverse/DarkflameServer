c689b3d3d1a6af247c5c72da25162dc32a23b852
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2022-07-16 21:39:13 -0700

    Parent and Child Deletion Improvements (#649)
    * Fix timers
    
    * Update Entity.cpp
    
    * Fix timers
    
    Fix timers
    
    Remove debug logs
    
    remove _dynamic
    
    * I like to move it move it
    
    * Child Deletion Improvements
    
    * Check bounds

--- dGame/Entity.h
+++ dGame/Entity.h
@@ -144,6 +144,7 @@ public:
 
 	void AddChild(Entity* child);
 	void RemoveChild(Entity* child);
+	void RemoveParent();
 	void AddTimer(std::string name, float time);
 	void AddCallbackTimer(float time, std::function<void()> callback);
 	bool HasTimer(const std::string& name);
