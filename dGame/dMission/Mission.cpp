cadc6a2a8589d70ffd7ae65e4aa5af01de9ba08a
Aaron Kimbre <aronwk.aaron@gmail.com>
2022-07-29 10:59:08 -0500

    fix the thing

--- dGame/dMission/Mission.cpp
+++ dGame/dMission/Mission.cpp
@@ -435,7 +435,7 @@ void Mission::YieldRewards() {
     int32_t coinsToSend = 0;
     if (info->LegoScore > 0) {
         eLootSourceType lootSource = info->isMission ? eLootSourceType::LOOT_SOURCE_MISSION : eLootSourceType::LOOT_SOURCE_ACHIEVEMENT;
-        if (characterComponent->GetLevel() >= dZoneManager::Instance()->GetMaxLevel() && !entity->GetCharacter()->GetPlayerFlag(ePlayerFlags::GIVE_USCORE_FROM_MISSIONS_AT_MAX_LEVEL)) {
+        if (levelComponent->GetLevel() >= dZoneManager::Instance()->GetMaxLevel() && !entity->GetCharacter()->GetPlayerFlag(ePlayerFlags::GIVE_USCORE_FROM_MISSIONS_AT_MAX_LEVEL)) {
             // If player is at the level cap and doesnt want to keep earning UScore at max level we convert it here.
             coinsToSend += info->LegoScore * dZoneManager::Instance()->GetLevelCapCurrencyConversion();
         } else {
