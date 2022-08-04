e6c7f744b5530ef1bdd50f7ba4548b452efcd676
Jett <55758076+Jettford@users.noreply.github.com>
2022-08-04 01:59:47 +0100

    Implement terrain file reading to generate navmeshes in the future (#703)
    * Implement terrain file reading to generate navmeshes in the future
    
    * Make Emo's suggested changes.

--- dNavigation/dNavMesh.cpp
+++ dNavigation/dNavMesh.cpp
@@ -1,10 +1,14 @@
 #include "dNavMesh.h"
 
+#include "RawFile.h"
+
 #include "Game.h"
 #include "dLogger.h"
 #include "dPlatforms.h"
 #include "NiPoint3.h"
 #include "BinaryIO.h"
+
+#include "dZoneManager.h"
 
 dNavMesh::dNavMesh(uint32_t zoneId) {
 	m_ZoneId = zoneId;
