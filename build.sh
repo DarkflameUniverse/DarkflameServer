7631074107ec1fad320eb98101c4b96b3f833c7f
HailStorm32 <demetriv.s.7@gmail.com>
2022-08-14 21:16:03 +0000

    Removed movement of binaries

--- build.sh
+++ build.sh
@@ -9,11 +9,6 @@ cmake ..
 if [[ "$OSTYPE" =~ ^msys ]]; then
 
 	cmake --build . --config Release
-	
-	# Move over binaries
-	if test -f "Release/MasterServer.exe"; then
-		mv Release/*.exe .
-	fi
 else
 
 	# Run make to build the project. To build utilizing multiple cores, append `-j` and the amount of cores to utilize, for example `make -j8`
