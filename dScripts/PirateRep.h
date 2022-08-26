150e7040687dc17abd275d2c411ca21d3395bbc3
Aaron Kimbre <aronwk.aaron@gmail.com>
2022-08-25 19:40:33 -0500

    Add pirate rep script

---
+++ dScripts/PirateRep.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "CppScripts.h"
+
+class PirateRep : public CppScripts::Script
+{
+public:
+	void OnMissionDialogueOK(Entity* self, Entity* target, int missionID, MissionState missionState) override;
+private:
+	const int m_PirateRepMissionID = 301;
+};
