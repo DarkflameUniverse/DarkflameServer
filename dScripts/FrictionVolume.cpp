be551707b8e5d8135bc6d7adb8a7b49d38ae151d
Aaron Kimbre <aronwk.aaron@gmail.com>
2022-08-27 23:27:39 -0500

    WIP

--- dScripts/FrictionVolume.cpp
+++ dScripts/FrictionVolume.cpp
@@ -1,14 +1,17 @@
 #include "FrictionVolume.h"
 #include "PhantomPhysicsComponent.h"
 #include "Entity.h"
+#include "dLogger.h"
 
 void FrictionVolume::OnStartup(Entity* self) {
+	Game::logger->Log("FrictionVolume", "LOT %i", self->GetLOT());
 	auto frictionAmount = self->GetVarAs<float>(m_FrictionVariable);
+	Game::logger->Log("FrictionVolume", "Lvl friction %f", frictionAmount);
 	if (!frictionAmount) frictionAmount = m_DefaultFriction;
-	auto* phanPhys = self->GetComponent<PhantomPhysicsComponent>();
-	if (!phanPhys) return;
+	auto* phantomPhysicsComponent = self->GetComponent<PhantomPhysicsComponent>();
+	if (!phantomPhysicsComponent) return;
 
-	phanPhys->SetPhysicsEffectActive(true);
-	phanPhys->SetEffectType(4); // this should be noted in a enum somewhere
-	phanPhys->SetDirectionalMultiplier(frictionAmount);
+	phantomPhysicsComponent->SetPhysicsEffectActive(true);
+	phantomPhysicsComponent->SetEffectType(4); // this should be noted in a enum somewhere
+	phantomPhysicsComponent->SetDirectionalMultiplier(frictionAmount);
 }
