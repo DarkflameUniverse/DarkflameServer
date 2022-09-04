5b59e6a38161bd0c0d4c60f73749795f3fea2407
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-09-04 03:08:10 -0700

    Formatting
    Note for future maintainers
    
    Add proper throw for systems with no zlib
    
    If the build system is not windows or mac there is no setup for zlib.  Throw an error if this happens.
    
    typo

--- dNet/WorldPackets.cpp
+++ dNet/WorldPackets.cpp
@@ -165,11 +165,11 @@ void WorldPackets::SendCreateCharacter(const SystemAddress& sysAddr, Entity* ent
 	delete reputation;
 
 	//Compress the data before sending:
-    const uint32_t reservedSize = ZCompression::GetMaxCompressedLength(data.GetNumberOfBytesUsed());
-    uint8_t* compressedData = static_cast<uint8_t*>(malloc(reservedSize));
+	const uint32_t reservedSize = ZCompression::GetMaxCompressedLength(data.GetNumberOfBytesUsed());
+	uint8_t* compressedData = static_cast<uint8_t*>(malloc(reservedSize));
 
 	// TODO There should be better handling here for not enough memory...
-    if (!compressedData) return;
+	if (!compressedData) return;
 
 	size_t size = ZCompression::Compress(data.GetData(), data.GetNumberOfBytesUsed(), compressedData, reservedSize);
 
