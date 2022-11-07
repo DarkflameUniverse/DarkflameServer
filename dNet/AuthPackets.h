374ac87b743181e52a56fc595335c0d01fead548
Jonathan Romano <jonathan@luxaritas.com>
2022-11-07 01:14:54 -0500

    Fix server type advertisement in handshake with custom auth port

--- dNet/AuthPackets.h
+++ dNet/AuthPackets.h
@@ -4,12 +4,13 @@
 #define _VARIADIC_MAX 10
 #include "dCommonVars.h"
 #include "dNetCommon.h"
+#include "dServer.h"
 
 class dServer;
 
 namespace AuthPackets {
 	void HandleHandshake(dServer* server, Packet* packet);
-	void SendHandshake(dServer* server, const SystemAddress& sysAddr, const std::string& nextServerIP, uint16_t nextServerPort);
+	void SendHandshake(dServer* server, const SystemAddress& sysAddr, const std::string& nextServerIP, uint16_t nextServerPort, const ServerType serverType);
 
 	void HandleLoginRequest(dServer* server, Packet* packet);
 	void SendLoginResponse(dServer* server, const SystemAddress& sysAddr, eLoginResponse responseCode, const std::string& errorMsg, const std::string& wServerIP, uint16_t wServerPort, std::string username);
