7b1c9709694d3e671989a9341edb75a5c3c3f853
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2022-11-22 18:11:16 -0800

    Add Remove Buff Behavior

---
+++ dGame/dBehaviors/RemoveBuffBehavior.cpp
@@ -0,0 +1,21 @@
+#include "RemoveBuffBehavior.h"
+
+#include "BehaviorBranchContext.h"
+#include "BehaviorContext.h"
+#include "EntityManager.h"
+#include "BuffComponent.h"
+
+void RemoveBuffBehavior::Handle(BehaviorContext* context, RakNet::BitStream* bitStream, BehaviorBranchContext branch) {
+	auto* entity = EntityManager::Instance()->GetEntity(context->caster);
+	if (!entity) return;
+
+	auto* buffComponent = entity->GetComponent<BuffComponent>();
+	if (!buffComponent) return;
+
+	buffComponent->RemoveBuff(m_BuffId);
+}
+
+void RemoveBuffBehavior::Load() {
+	this->m_RemoveImmunity = GetBoolean("remove_immunity");
+	this->m_BuffId = GetInt("buff_id");
+}
