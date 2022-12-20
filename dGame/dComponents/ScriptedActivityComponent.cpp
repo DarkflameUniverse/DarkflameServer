fecb304a32b827c6990645fe2b149f84358c8616
David Markowitz <EmosewaMC@gmail.com>
2022-12-20 04:04:33 -0800

    Fix lobbies

--- dGame/dComponents/ScriptedActivityComponent.cpp
+++ dGame/dComponents/ScriptedActivityComponent.cpp
@@ -208,7 +208,7 @@ void ScriptedActivityComponent::PlayerLeave(LWOOBJID playerID) {
 }
 
 void ScriptedActivityComponent::Update(float deltaTime) {
-
+	std::vector<Lobby*> lobbiesToRemove{};
 	// Ticks all the lobbies, not applicable for non-instance activities
 	for (Lobby* lobby : m_Queue) {
 		for (LobbyPlayer* player : lobby->players) {
@@ -217,6 +217,11 @@ void ScriptedActivityComponent::Update(float deltaTime) {
 				PlayerLeave(player->entityID);
 				return;
 			}
+		}
+
+		if (lobby->players.empty()) {
+			lobbiesToRemove.push_back(lobby);
+			continue;
 		}
 
 		// Update the match time for all players
@@ -262,12 +267,16 @@ void ScriptedActivityComponent::Update(float deltaTime) {
 		// The timer has elapsed, start the instance
 		if (lobby->timer <= 0.0f) {
 			Game::logger->Log("ScriptedActivityComponent", "Setting up instance.");
-
 			ActivityInstance* instance = NewInstance();
 			LoadPlayersIntoInstance(instance, lobby->players);
-			RemoveLobby(lobby);
 			instance->StartZone();
+			lobbiesToRemove.push_back(lobby);
 		}
+	}
+
+	while (!lobbiesToRemove.empty()) {
+		RemoveLobby(lobbiesToRemove.front());
+		lobbiesToRemove.erase(lobbiesToRemove.begin());
 	}
 }
 
