2c8a9932987f33a00e37e8c428defe965577a811
David Markowitz <EmosewaMC@gmail.com>
2022-12-26 00:19:20 -0800

    Update FdbToSqlite.cpp

--- dCommon/FdbToSqlite.cpp
+++ dCommon/FdbToSqlite.cpp
@@ -63,6 +63,7 @@ int64_t FdbToSqlite::Convert::ReadInt64(std::istream& cdClientBuffer) {
 	int64_t value{};
 	BinaryIO::BinaryRead(cdClientBuffer, value);
 
+	cdClientBuffer.seekg(prevPosition);
 	return value;
 }
 
