9135eb1efcf0a632653be4ca3392a1f5d7bd037f
Aaron Kimbre <aronwk.aaron@gmail.com>
2023-01-19 11:06:56 -0600

    clone

--- dGame/EntityManager.cpp
+++ dGame/EntityManager.cpp
@@ -70,9 +70,9 @@ void EntityManager::Initialize() {
 	auto hcDropInv = Game::config->GetValue("hardcore_dropinventory_on_death");
 	m_HardcoreDropinventoryOnDeath = hcDropInv.empty() ? false : (hcDropInv == "1");
 
-	// If instance is not zero, then hardcore mode is disabled
+	// If cloneID is not zero, then hardcore mode is disabled
 	// aka minigames and props
-	if (dZoneManager::Instance()->GetZoneID().GetInstanceID() != 0) m_HardcoreMode = false;
+	if (dZoneManager::Instance()->GetZoneID().GetCloneID() != 0) m_HardcoreMode = false;
 }
 
 EntityManager::~EntityManager() {
