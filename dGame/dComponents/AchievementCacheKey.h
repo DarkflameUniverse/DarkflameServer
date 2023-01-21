3f47d1032a2bba0d55a121f9851b8105fa040f2a
David Markowitz <EmosewaMC@gmail.com>
2023-01-21 01:07:44 -0800

    Fix hash collisions in achievements

---
+++ dGame/dComponents/AchievementCacheKey.h
@@ -0,0 +1,43 @@
+#ifndef __ACHIEVEMENTCACHEKEY__H__
+#define __ACHIEVEMENTCACHEKEY__H__
+
+class AchievementCacheKey {
+public:
+	AchievementCacheKey() {
+		targets = "";
+		value = 0;
+		type = MissionTaskType::MISSION_TASK_TYPE_UNKNOWN;
+	};
+
+	bool operator==(const AchievementCacheKey& point) const {
+		return this->targets == point.targets && this->value == point.value && this->type == point.type;
+	};
+	void SetTargets(const std::string value) { this->targets = value; };
+	void SetValue(uint32_t value) { this->value = value; };
+	void SetType(MissionTaskType value) { this->type = value; };
+
+	std::string GetTargets() const { return this->targets; };
+	uint32_t GetValue() const { return this->value; };
+	MissionTaskType GetType() const { return this->type; };
+private:
+	std::string targets;
+	uint32_t value;
+	MissionTaskType type;
+
+};
+
+// Specialization of hash for the above class
+namespace std {
+	template<>
+	struct hash<AchievementCacheKey> {
+		size_t operator()(const AchievementCacheKey& key) const {
+			size_t hash = 0;
+			GeneralUtils::hash_combine(hash, key.GetType());
+			GeneralUtils::hash_combine(hash, key.GetValue());
+			GeneralUtils::hash_combine(hash, key.GetTargets());
+			return hash;
+		};
+	};
+};
+
+#endif  //!__ACHIEVEMENTCACHEKEY__H__
