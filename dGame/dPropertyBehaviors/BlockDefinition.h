d07c46839c0eb61cfeb263613ce5cc39105ea099
David Markowitz <EmosewaMC@gmail.com>
2023-02-09 04:38:22 -0800

    Update BlockDefinition.h

--- dGame/dPropertyBehaviors/BlockDefinition.h
+++ dGame/dPropertyBehaviors/BlockDefinition.h
@@ -5,7 +5,6 @@
 
 class AMFArrayValue;
 
-// Minimum and maximum values are always ints, but used for different things in different contexts
 class BlockDefinition {
 public:
 	BlockDefinition(std::string defaultValue = "", float minimumValue = 0.0f, float maximumValue = 0.0f);
