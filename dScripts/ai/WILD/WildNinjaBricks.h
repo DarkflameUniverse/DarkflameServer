da6ca82ae23388606993a67112a17595bffe2dcd
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2023-04-17 23:40:33 -0700

    add missing overrides (#1060)

--- dScripts/ai/WILD/WildNinjaBricks.h
+++ dScripts/ai/WILD/WildNinjaBricks.h
@@ -3,7 +3,7 @@
 
 class WildNinjaBricks : public CppScripts::Script {
 public:
-	void OnStartup(Entity* self);
+	void OnStartup(Entity* self) override;
 	void OnNotifyObject(Entity* self, Entity* sender, const std::string& name, int32_t param1 = 0, int32_t param2 = 0) override;
 };
 
