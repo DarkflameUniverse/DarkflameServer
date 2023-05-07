0cd887e7b1c42844f21c70ac0f6d5249da5785f5
David Markowitz <EmosewaMC@gmail.com>
2023-05-07 00:10:30 -0700

    Formatting pass

--- tests/dCommonTests/HeaderSkipTest.cpp
+++ tests/dCommonTests/HeaderSkipTest.cpp
@@ -8,7 +8,7 @@
 
 TEST(dCommonTests, HeaderSkipExcessTest) {
 	PacketUniquePtr packet = std::make_unique<Packet>();
-	unsigned char headerAndData[] = {0x53, 0x02, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00}; // positive
+	unsigned char headerAndData[] = { 0x53, 0x02, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00 }; // positive
 	packet->data = headerAndData;
 	packet->length = sizeof(headerAndData);
 	CINSTREAM_SKIP_HEADER;
@@ -18,7 +18,7 @@ TEST(dCommonTests, HeaderSkipExcessTest) {
 
 TEST(dCommonTests, HeaderSkipExactDataTest) {
 	PacketUniquePtr packet = std::make_unique<Packet>();
-	unsigned char header[] = {0x53, 0x02, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00}; // positive
+	unsigned char header[] = { 0x53, 0x02, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00 }; // positive
 	packet->data = header;
 	packet->length = sizeof(header);
 	CINSTREAM_SKIP_HEADER;
@@ -28,7 +28,7 @@ TEST(dCommonTests, HeaderSkipExactDataTest) {
 
 TEST(dCommonTests, HeaderSkipNotEnoughDataTest) {
 	PacketUniquePtr packet = std::make_unique<Packet>();
-	unsigned char notEnoughData[] = {0x53, 0x02, 0x00, 0x07, 0x00, 0x00}; // negative
+	unsigned char notEnoughData[] = { 0x53, 0x02, 0x00, 0x07, 0x00, 0x00 }; // negative
 	packet->data = notEnoughData;
 	packet->length = sizeof(notEnoughData);
 	CINSTREAM_SKIP_HEADER;
