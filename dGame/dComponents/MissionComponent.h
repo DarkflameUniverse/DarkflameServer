950a1fe4231d4b23d2f54cdc9316686996a6ea31
EmosewaMC <39972741+EmosewaMC@users.noreply.github.com>
2023-07-05 19:32:28 -0700

    MissionComponent pass

--- dGame/dComponents/MissionComponent.h
+++ dGame/dComponents/MissionComponent.h
@@ -39,35 +39,35 @@ public:
 	 * Returns all the missions for this entity, mapped by mission ID
 	 * @return the missions for this entity, mapped by mission ID
 	 */
-	const std::unordered_map<uint32_t, Mission*>& GetMissions() const;
+	const std::unordered_map<uint32_t, Mission*>& GetMissions() const { return m_Missions; };
 
 	/**
 	 * Returns the mission for the given mission ID, if it exists
 	 * @param missionId the id of the mission to get
 	 * @return the mission for the given mission ID
 	 */
-	Mission* GetMission(uint32_t missionId) const;
+	Mission* GetMission(const uint32_t missionId) const;
 
 	/**
 	 * Returns the current state of the entities progression for the mission of the specified ID
 	 * @param missionId the ID of the mission to get the mission state for
 	 * @return the mission state of the mission specified by the ID
 	 */
-	eMissionState GetMissionState(uint32_t missionId) const;
+	eMissionState GetMissionState(const uint32_t missionId) const;
 
 	/**
 	 * Checks if the entity has all the requirements for accepting the mission specified by the ID.
 	 * @param missionId the mission ID to check for if the character may accept it
 	 * @return whether this entity can accept the mission represented by the given mission ID
 	 */
-	bool CanAccept(uint32_t missionId) const;
+	bool CanAccept(const uint32_t missionId) const;
 
 	/**
 	 * Accepts the mission specified by the ID, if the entity may accept it. Also stores it in the mission inventory.
 	 * @param missionId the ID of the mission to accept
 	 * @param skipChecks skips the checks for the mission prerequisites
 	 */
-	void AcceptMission(uint32_t missionId, bool skipChecks = false);
+	void AcceptMission(const uint32_t missionId, const bool skipChecks = false);
 
 	/**
 	 * Completes the mission specified by the given ID, if the entity has fulfilled all progress requirements.
@@ -75,13 +75,13 @@ public:
 	 * @param skipChecks skips the checks for having completed all of the mission tasks
 	 * @param yieldRewards whether to yield mission rewards, currently unused
 	 */
-	void CompleteMission(uint32_t missionId, bool skipChecks = false, bool yieldRewards = true);
+	void CompleteMission(const uint32_t missionId, const bool skipChecks = false, const bool yieldRewards = true);
 
 	/**
 	 * Removes the mission from the entities' mission chain. Not used for normal gameplay but useful for debugging.
 	 * @param missionId the ID of the mission to remove
 	 */
-	void RemoveMission(uint32_t missionId);
+	void RemoveMission(const uint32_t missionId);
 
 	/**
 	 * Attempts to progress mission tasks for a given type using parameters to progress. Note that this function is
@@ -94,7 +94,7 @@ public:
 	 * @param count the number to progress by, for example the number of items
 	 * @param ignoreAchievements do not progress achievements
 	 */
-	void Progress(eMissionTaskType type, int32_t value, LWOOBJID associate = 0, const std::string& targets = "", int32_t count = 1, bool ignoreAchievements = false);
+	void Progress(const eMissionTaskType type, const int32_t value, const LWOOBJID& associate = 0, const std::string& targets = "", const int32_t count = 1, const bool ignoreAchievements = false);
 
 	/**
 	 * Forces progression for a mission and task, ignoring checks
@@ -103,7 +103,7 @@ public:
 	 * @param value the value to progress with
 	 * @param acceptMission accept the mission if it was not already accepted
 	 */
-	void ForceProgress(uint32_t missionId, uint32_t taskId, int32_t value, bool acceptMission = true);
+	void ForceProgress(const uint32_t missionId, const uint32_t taskId, const int32_t value, const bool acceptMission = true);
 
 	/**
 	 * Forces progress for all tasks of a certain type that belong to the same mission
@@ -112,7 +112,7 @@ public:
 	 * @param value the value to progress with
 	 * @param acceptMission accept the mission if it wasn't already
 	 */
-	void ForceProgressTaskType(uint32_t missionId, uint32_t taskType, int32_t value, bool acceptMission = true);
+	void ForceProgressTaskType(const uint32_t missionId, const uint32_t taskType, const int32_t value, const bool acceptMission = true);
 
 	/**
 	 * Force progresses by checking the value and progressing by 1
@@ -121,7 +121,7 @@ public:
 	 * @param value the value to check the mission values before progressing
 	 * @param acceptMission accept the mission if it wasn't already
 	 */
-	void ForceProgressValue(uint32_t missionId, uint32_t taskType, int32_t value, bool acceptMission = true);
+	void ForceProgressValue(const uint32_t missionId, const uint32_t taskType, const int32_t value, const bool acceptMission = true);
 
 	/**
 	 * Returns client database mission information for a mission
@@ -129,7 +129,7 @@ public:
 	 * @param result the result to store the information in
 	 * @return true if the information was succesfully retrieved, false otherwise
 	 */
-	bool GetMissionInfo(uint32_t missionId, CDMissions& result);
+	bool GetMissionInfo(const uint32_t missionId, CDMissions& result) const;
 
 	/**
 	 * Checks if there's any achievements we might be able to accept for the given parameters
@@ -141,34 +141,34 @@ public:
 	 * @param count the number of values to progress by (differs by task type)
 	 * @return true if a achievement was accepted, false otherwise
 	 */
-	bool LookForAchievements(eMissionTaskType type, int32_t value, bool progress = true, LWOOBJID associate = LWOOBJID_EMPTY, const std::string& targets = "", int32_t count = 1);
+	bool LookForAchievements(const eMissionTaskType type, const int32_t value, const bool progress = true, const LWOOBJID& associate = LWOOBJID_EMPTY, const std::string& targets = "", const int32_t count = 1);
 
 	/**
 	 * Checks if there's a mission active that requires the collection of the specified LOT
 	 * @param lot the LOT to check for
 	 * @return if there's a mission active that requires the collection of the specified LOT
 	 */
-	bool RequiresItem(LOT lot);
+	bool RequiresItem(const LOT lot);
 
 	/**
 	 * Collects a collectable for the entity, unrendering it for the entity
 	 * @param collectibleID the ID of the collectable to add
 	 */
-	void AddCollectible(int32_t collectibleID);
+	void AddCollectible(const int32_t collectibleID);
 
 	/**
 	 * Checks if the entity already has a collectible of the specified ID
 	 * @param collectibleID the ID of the collectible to check
 	 * @return if the entity already has a collectible of the specified ID
 	 */
-	bool HasCollectible(int32_t collectibleID);
+	bool HasCollectible(const int32_t collectibleID) const;
 
 	/**
 	 * Checks if the entity has a certain mission in its inventory
 	 * @param missionId the ID of the mission to check
 	 * @return if the entity has a certain mission in its inventory
 	 */
-	bool HasMission(uint32_t missionId);
+	bool HasMission(const uint32_t missionId) const;
 
 private:
 	/**
@@ -189,7 +189,7 @@ private:
 	 * @param targets optional targets to progress with
 	 * @return list of mission IDs (achievements) that can be progressed for the given parameters
 	 */
-	static const std::vector<uint32_t>& QueryAchievements(eMissionTaskType type, int32_t value, const std::string targets);
+	static const std::vector<uint32_t>& QueryAchievements(const eMissionTaskType type, const int32_t value, const std::string& targets);
 
 	/**
 	 * As achievements can be hard to query, we here store a list of all the mission IDs that can be unlocked for a
