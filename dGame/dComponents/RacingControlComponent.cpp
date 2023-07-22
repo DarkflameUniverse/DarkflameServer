342da566788474e62436e280a1444f3b156c8e79
TAHuntling <38479763+TAHuntling@users.noreply.github.com>
2023-07-21 19:37:31 -0500

    Added Player Rewards for Solo Racing (#1150)
    * Added Player Rewards for Solo Racing
    
    * Fixed Dual PlayersRating multiplication
    
    * Checking for solo player fixed
    
    * Another change to fix issues

--- dGame/dComponents/RacingControlComponent.cpp
+++ dGame/dComponents/RacingControlComponent.cpp
@@ -374,8 +374,12 @@ void RacingControlComponent::HandleMessageBoxResponse(Entity* player, int32_t bu
 		data->collectedRewards = true;
 
 		// Calculate the score, different loot depending on player count
-		const auto score = m_LoadedPlayers * 10 + data->finished;
+		auto playersRating = m_LoadedPlayers;
+		if(m_LoadedPlayers == 1 && m_SoloRacing) {
+			playersRating *= 2;
+		}
 
+        const auto score = playersRating * 10 + data->finished;
 		LootGenerator::Instance().GiveActivityLoot(player, m_Parent, m_ActivityID, score);
 
 		// Giving rewards
