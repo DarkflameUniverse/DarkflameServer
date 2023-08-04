e5b69745aa779903a6450c14a49730c7eb48e9fb
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2023-08-03 19:38:33 -0700

    feat: Remove NET_VERSION define (#1153)
    * Remove NET_VERSION define
    
    I get to test this on 4 different platforms.  yay...
    
    * move stuff around
    
    finally fixed you docker
    
    i love this
    
    Update Dockerfile
    
    Update Dockerfile
    
    change type
    
    Update AuthPackets.cpp
    
    Update AuthServer.cpp
    
    * Update CMakeVariables.txt
    
    * Update sharedconfig.ini

--- dNet/AuthPackets.cpp
+++ dNet/AuthPackets.cpp
@@ -40,7 +40,12 @@ void AuthPackets::HandleHandshake(dServer* server, Packet* packet) {
 void AuthPackets::SendHandshake(dServer* server, const SystemAddress& sysAddr, const std::string& nextServerIP, uint16_t nextServerPort, const ServerType serverType) {
 	RakNet::BitStream bitStream;
 	PacketUtils::WriteHeader(bitStream, eConnectionType::SERVER, eServerMessageType::VERSION_CONFIRM);
-	bitStream.Write<unsigned int>(NET_VERSION);
+	uint32_t netVersion;
+	if (!GeneralUtils::TryParse(Game::config->GetValue("client_net_version"), netVersion)) {
+		Game::logger->Log("AuthPackets", "Failed to parse client_net_version. Cannot authenticate to %s:%i", nextServerIP.c_str(), nextServerPort);
+		return;
+	}
+	bitStream.Write<uint32_t>(netVersion);
 	bitStream.Write(uint32_t(0x93));
 
 	if (serverType == ServerType::Auth) bitStream.Write(uint32_t(1)); //Conn: auth
