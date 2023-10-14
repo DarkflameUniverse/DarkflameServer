ba48973eafedffcb305f0c3bee20961afcb1de98
aronwk-aaron <aronwk.aaron@gmail.com>
2023-10-13 20:49:05 -0500

    remove rot adjust chance

--- dScripts/newcontent/server/SpawnSkeletonOnDeath.cpp
+++ dScripts/newcontent/server/SpawnSkeletonOnDeath.cpp
@@ -10,15 +10,13 @@ void SpawnSkeletonOnDeath::OnDie(Entity* self, Entity* killer) {
 		EntityInfo info{};
 		info.lot = 20083;
 		info.pos = self->GetPosition();
-		info.rot = self->GetRotation();
 		info.spawnerID = killer->GetObjectID();
 		auto skellyboi = Game::entityManager->CreateEntity(info, nullptr, killer);
 		Game::entityManager->ConstructEntity(skellyboi);
-	} else if (chance > 20 && chance < 23) {
+	} else if (chance == 20) {
 		EntityInfo info{};
 		info.lot = 20085;
 		info.pos = self->GetPosition();
-		info.rot = self->GetRotation();
 		info.spawnerID = killer->GetObjectID();
 		auto skellyboi = Game::entityManager->CreateEntity(info, nullptr, killer);
 		Game::entityManager->ConstructEntity(skellyboi);
