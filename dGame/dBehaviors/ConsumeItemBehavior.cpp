759c33589a352d0d33dc73fd9d32714e90f1f886
Aaron Kimbre <aronwk.aaron@gmail.com>
2023-11-13 19:35:58 -0600

    remove logs

--- dGame/dBehaviors/ConsumeItemBehavior.cpp
+++ dGame/dBehaviors/ConsumeItemBehavior.cpp
@@ -4,10 +4,8 @@
 #include "InventoryComponent.h"
 
 void ConsumeItemBehavior::Handle(BehaviorContext* context, RakNet::BitStream* bitStream, BehaviorBranchContext branch) {
-	LOG("Handle Consume Item");
 	auto action_to_cast = m_ActionNotConsumed;
 	if (this->m_ConsumeLOT != -1) {
-		LOG("Consumming %i of %i", m_NumToConsume, m_ConsumeLOT);
 		auto caster = Game::entityManager->GetEntity(context->caster);
 		if (!caster) return;
 
@@ -15,20 +13,15 @@ void ConsumeItemBehavior::Handle(BehaviorContext* context, RakNet::BitStream* bi
 		if (!inventoryComponent) return;
 
 		if (inventoryComponent->RemoveItem(this->m_ConsumeLOT, this->m_NumToConsume, eInventoryType::INVALID, false, true)){
-			LOG("Successfully consumes item!");
 			action_to_cast = m_ActionConsumed;
 		}
 	}
-	LOG("action %i", action_to_cast->m_behaviorId);
 	if(action_to_cast) action_to_cast->Handle(context, bitStream, branch);
 }
 
 void ConsumeItemBehavior::Sync(BehaviorContext* context, RakNet::BitStream* bitStream, BehaviorBranchContext branch) {
-	LOG("Sync consume item");
 	Handle(context, bitStream, branch);
 }
-
-
 
 void ConsumeItemBehavior::Load() {
 	this->m_ConsumeLOT = GetInt("consume_lot", -1);
