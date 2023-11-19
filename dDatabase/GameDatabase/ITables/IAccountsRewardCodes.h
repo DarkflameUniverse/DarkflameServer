0cb64d3209ee8a90a1bbbaa386fee3696ecf2e1f
Aaron Kimbre <aronwk.aaron@gmail.com>
2023-11-19 03:34:38 -0600

    newline

--- dDatabase/GameDatabase/ITables/IAccountsRewardCodes.h
+++ dDatabase/GameDatabase/ITables/IAccountsRewardCodes.h
@@ -9,4 +9,5 @@ public:
 	virtual void InsertRewardCode(const uint32_t account_id, const uint32_t reward_code) = 0;
 	virtual std::vector<uint32_t> GetRewardCodesByAccountID(const uint32_t account_id) = 0;
 };
+
 #endif  //!__IACCOUNTSREWARDCODES__H__
