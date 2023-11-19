c8b624c3dd313fa9c0457451392d403a7db6f03b
jadebenn <jonahbenn@yahoo.com>
2023-11-19 14:06:28 -0600

    minor changes

--- dScripts/02_server/Map/General/PetDigServer.cpp
+++ dScripts/02_server/Map/General/PetDigServer.cpp
@@ -113,7 +113,7 @@ void PetDigServer::OnUse(Entity* self, Entity* user) {
 		if (!destroyableComponent) return;
 		
 		auto imagination = destroyableComponent->GetImagination();
-		if (imagination == 0) return; // TODO: Check if there was special behavior for this in the live game
+		if (imagination == 0) return; // TODO: Check if there was special behavior for this in the live game (PR_NEED_IMAGINATION)
 
 		petComponent->SetTreasureTime(2.0f); // TODO: Get rid of this magic number
 
