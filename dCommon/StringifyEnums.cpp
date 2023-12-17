33fa5f8d2fc273ede9fffbc15a62d9a5335ffe6a
jadebenn <jadebenn@users.noreply.github.com>
2023-12-17 05:19:37 -0600

    moved stringify code to dCommon

--- dCommon/StringifyEnums.cpp
+++ dCommon/StringifyEnums.cpp
@@ -1,3 +1,5 @@
+// Source - https://mariusbancila.ro/blog/2023/08/17/how-to-convert-an-enum-to-string-in-cpp/
+
 #define GENERATE_ENUM_STRINGS  // Start string generation
 
 #include "eGameMessageType.h"
