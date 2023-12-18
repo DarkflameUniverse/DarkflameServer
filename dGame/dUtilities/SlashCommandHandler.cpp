ed10570c72599d08ec51c879937c52625d71411c
jadebenn <jonahbenn@yahoo.com>
2023-12-17 19:00:48 -0600

    fixed VE zone bug and added config option to renable it

--- dGame/dUtilities/SlashCommandHandler.cpp
+++ dGame/dUtilities/SlashCommandHandler.cpp
@@ -288,11 +288,14 @@ void SlashCommandHandler::HandleChatCommand(const std::u16string& command, Entit
 		return;
 	}
 
-	if (chatCommand == "leave-zone") {
+	if (chatCommand == "leave-zone" || chatCommand == "leavezone") {
 		const auto currentZone = Game::zoneManager->GetZone()->GetZoneID().GetMapID();
+		const auto VeZoneBugEnabled = Game::config->GetValue("enable_venture_explorer_bug");
+		LWOMAPID newZone;
 
-		LWOMAPID newZone = 0;
-		if (currentZone % 100 == 0) {
+		if (currentZone == 1001 && VeZoneBugEnabled != "1") {
+			newZone = 1100; // Send to AG if we're in a Return to Venture Explorer instance
+		} else if (currentZone % 100 == 0) {
 			ChatPackets::SendSystemMessage(sysAddr, u"You are not in an instanced zone.");
 			return;
 		} else {
