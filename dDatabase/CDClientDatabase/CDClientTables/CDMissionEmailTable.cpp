9c22b4cb81315db3f19b9b783dace3054042f8dc
jadebenn <jonahebenn@yahoo.com>
2023-12-26 16:33:52 -0600

    no getBoolField method exists

--- dDatabase/CDClientDatabase/CDClientTables/CDMissionEmailTable.cpp
+++ dDatabase/CDClientDatabase/CDClientTables/CDMissionEmailTable.cpp
@@ -25,7 +25,7 @@ void CDMissionEmailTable::LoadValuesFromDatabase() {
 		entry.notificationGroup = tableData.getIntField("notificationGroup", -1);
 		entry.missionID = tableData.getIntField("missionID", -1);
 		entry.attachmentLOT = tableData.getIntField("attachmentLOT", 0);
-		entry.localize = tableData.getBoolField("localize", -1);
+		entry.localize = (bool)tableData.getIntField("localize", -1);
 		entry.locStatus = tableData.getIntField("locStatus", -1);
 		entry.gate_version = tableData.getStringField("gate_version", "");
 
@@ -50,3 +50,4 @@ std::vector<CDMissionEmail> CDMissionEmailTable::Query(std::function<bool(CDMiss
 const std::vector<CDMissionEmail>& CDMissionEmailTable::GetEntries() const {
 	return this->entries;
 }
+
