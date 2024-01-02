06c0ed36e629266e7eafeca9d979f7e2e2f83567
David Markowitz <EmosewaMC@gmail.com>
2024-01-02 02:47:30 -0800

    Update MasterServer.cpp

--- dMasterServer/MasterServer.cpp
+++ dMasterServer/MasterServer.cpp
@@ -73,7 +73,7 @@ int main(int argc, char** argv) {
 #endif
 
 	//Triggers the shutdown sequence at application exit
-	std::atexit([]() { printf("\n"); ShutdownSequence(); });
+	std::atexit([]() { ShutdownSequence(); });
 	std::signal(SIGINT, Game::OnSignal);
 	std::signal(SIGTERM, Game::OnSignal);
 
