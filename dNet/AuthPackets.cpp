ed251b45e43af6c383a6f86836d22b9107ee5597
Aaron Kimbre <aronwk.aaron@gmail.com>
2024-01-06 01:28:13 -0600

    fix the stupid define

--- dNet/AuthPackets.cpp
+++ dNet/AuthPackets.cpp
@@ -95,7 +95,7 @@ void AuthPackets::HandleLoginRequest(dServer* server, Packet* packet) {
 	CINSTREAM_SKIP_HEADER;
 
 	std::vector<Stamp> stamps;
-	stamps.push_back(Stamp(eStamps::START, 0, time(nullptr)));
+	stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_START, 0, time(nullptr)));
 
 	LUWString usernameLUString(33);
 	inStream.Read(usernameLUString);
@@ -111,7 +111,7 @@ void AuthPackets::HandleLoginRequest(dServer* server, Packet* packet) {
 	ClientOS clientOS;
 	inStream.Read(clientOS);
 	LOG("Operating System: %s", StringifiedEnum::ToString(clientOS).data());
-	stamps.push_back(Stamp(eStamps::CLIENT_OS, 0, time(nullptr)));
+	stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_CLIENT_OS, 0, time(nullptr)));
 
 	LUWString memoryStats(256);
 	inStream.Read(memoryStats);
@@ -150,7 +150,7 @@ void AuthPackets::HandleLoginRequest(dServer* server, Packet* packet) {
 
 	if (!accountInfo) {
 		LOG("No user by name %s found!", username.c_str());
-		stamps.push_back(Stamp(eStamps::ERROR, 1, time(nullptr)));
+		stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_ERROR, 1, time(nullptr)));
 		AuthPackets::SendLoginResponse(server, packet->systemAddress, eLoginResponse::INVALID_USER, "", "", 2001, username, stamps);
 		return;
 	}
@@ -166,7 +166,7 @@ void AuthPackets::HandleLoginRequest(dServer* server, Packet* packet) {
 	if (Game::config->GetValue("dont_use_keys") != "1" && accountInfo->maxGmLevel == eGameMasterLevel::CIVILIAN) {
 		//Check to see if we have a play key:
 		if (accountInfo->playKeyId == 0) {
-			stamps.push_back(Stamp(eStamps::ERROR, 1, time(nullptr)));
+			stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_ERROR, 1, time(nullptr)));
 			AuthPackets::SendLoginResponse(server, packet->systemAddress, eLoginResponse::PERMISSIONS_NOT_HIGH_ENOUGH, "Your account doesn't have a play key associated with it!", "", 2001, username, stamps);
 			LOG("User %s tried to log in, but they don't have a play key.", username.c_str());
 			return;
@@ -176,29 +176,29 @@ void AuthPackets::HandleLoginRequest(dServer* server, Packet* packet) {
 		auto playKeyStatus = Database::Get()->IsPlaykeyActive(accountInfo->playKeyId);
 
 		if (!playKeyStatus) {
-			stamps.push_back(Stamp(eStamps::ERROR, 1, time(nullptr)));
+			stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_ERROR, 1, time(nullptr)));
 			AuthPackets::SendLoginResponse(server, packet->systemAddress, eLoginResponse::PERMISSIONS_NOT_HIGH_ENOUGH, "Your account doesn't have a valid play key associated with it!", "", 2001, username, stamps);
 			return;
 		}
 
 		if (!playKeyStatus.value()) {
-			stamps.push_back(Stamp(eStamps::ERROR, 1, time(nullptr)));
+			stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_ERROR, 1, time(nullptr)));
 			AuthPackets::SendLoginResponse(server, packet->systemAddress, eLoginResponse::PERMISSIONS_NOT_HIGH_ENOUGH, "Your play key has been disabled.", "", 2001, username, stamps);
 			LOG("User %s tried to log in, but their play key was disabled", username.c_str());
 			return;
 		}
 	} else if (Game::config->GetValue("dont_use_keys") == "1" || accountInfo->maxGmLevel > eGameMasterLevel::CIVILIAN){
-		stamps.push_back(Stamp(eStamps::BYPASS, 1, time(nullptr)));
+		stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_BYPASS, 1, time(nullptr)));
 	}
 
 	if (accountInfo->banned) {
-		stamps.push_back(Stamp(eStamps::ERROR, 1, time(nullptr)));
+		stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_ERROR, 1, time(nullptr)));
 		AuthPackets::SendLoginResponse(server, packet->systemAddress, eLoginResponse::BANNED, "", "", 2001, username, stamps);
 		return;
 	}
 
 	if (accountInfo->locked) {
-		stamps.push_back(Stamp(eStamps::ERROR, 1, time(nullptr)));
+		stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_ERROR, 1, time(nullptr)));
 		AuthPackets::SendLoginResponse(server, packet->systemAddress, eLoginResponse::ACCOUNT_LOCKED, "", "", 2001, username, stamps);
 		return;
 	}
@@ -206,18 +206,18 @@ void AuthPackets::HandleLoginRequest(dServer* server, Packet* packet) {
 	bool loginSuccess = ::bcrypt_checkpw(password.GetAsString().c_str(), accountInfo->bcryptPassword.c_str()) == 0;
 
 	if (!loginSuccess) {
-		stamps.push_back(Stamp(eStamps::ERROR, 1, time(nullptr)));
+		stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_ERROR, 1, time(nullptr)));
 		AuthPackets::SendLoginResponse(server, packet->systemAddress, eLoginResponse::WRONG_PASS, "", "", 2001, username, stamps);
 		LOG("Wrong password used");
 	} else {
 		SystemAddress system = packet->systemAddress; //Copy the sysAddr before the Packet gets destroyed from main
 
 		if (!server->GetIsConnectedToMaster()) {
-			stamps.push_back(Stamp(eStamps::WORLD_DISCONNECT, 1, time(nullptr)));
+			stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_WORLD_DISCONNECT, 1, time(nullptr)));
 			AuthPackets::SendLoginResponse(server, system, eLoginResponse::GENERAL_FAILED, "", "", 0, username, stamps);
 			return;
 		}
-		stamps.push_back(Stamp(eStamps::WORLD_SESSION_CONFIRM_TO_AUTH, 1, time(nullptr)));
+		stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_WORLD_SESSION_CONFIRM_TO_AUTH, 1, time(nullptr)));
 		ZoneInstanceManager::Instance()->RequestZoneTransfer(server, 0, 0, false, [system, server, username, stamps](bool mythranShift, uint32_t zoneID, uint32_t zoneInstance, uint32_t zoneClone, std::string zoneIP, uint16_t zonePort) mutable {
 			AuthPackets::SendLoginResponse(server, system, eLoginResponse::SUCCESS, "", zoneIP, zonePort, username, stamps);
 			});
@@ -229,7 +229,7 @@ void AuthPackets::HandleLoginRequest(dServer* server, Packet* packet) {
 }
 
 void AuthPackets::SendLoginResponse(dServer* server, const SystemAddress& sysAddr, eLoginResponse responseCode, const std::string& errorMsg, const std::string& wServerIP, uint16_t wServerPort, std::string username, std::vector<Stamp>& stamps) {
-	stamps.push_back(Stamp(eStamps::IM_LOGIN_START, 1, time(nullptr)));
+	stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_IM_LOGIN_START, 1, time(nullptr)));
 	RakNet::BitStream loginResponse;
 	BitStreamUtils::WriteHeader(loginResponse, eConnectionType::CLIENT, eClientMessageType::LOGIN_RESPONSE);
 
@@ -292,7 +292,7 @@ void AuthPackets::SendLoginResponse(dServer* server, const SystemAddress& sysAdd
 	loginResponse.Write<uint16_t>(errorMsg.length());
 	loginResponse.Write(LUWString(errorMsg, static_cast<uint32_t>(errorMsg.length())));
 
-	stamps.push_back(Stamp(eStamps::WORLD_COMMUNICATION_FINISH, 1, time(nullptr)));
+	stamps.push_back(Stamp(eStamps::PASSPORT_AUTH_WORLD_COMMUNICATION_FINISH, 1, time(nullptr)));
 
 	loginResponse.Write<uint32_t>((sizeof(Stamp) * stamps.size()) + sizeof(uint32_t));
 	for (auto& stamp : stamps) stamp.Serialize(&loginResponse);
