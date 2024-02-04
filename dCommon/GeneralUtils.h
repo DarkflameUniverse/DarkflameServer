0ab30c7f94c4c6655fa95ea02057416da4d0c114
jadebenn <jonahbenn@yahoo.com>
2024-02-04 12:23:13 -0600

    hash is usally but not always noexcept, so the specifier should go

--- dCommon/GeneralUtils.h
+++ dCommon/GeneralUtils.h
@@ -244,7 +244,7 @@ namespace GeneralUtils {
 
 	// From boost::hash_combine
 	template <class T>
-	constexpr void hash_combine(std::size_t& s, const T& v) noexcept {
+	constexpr void hash_combine(std::size_t& s, const T& v) {
 		std::hash<T> h;
 		s ^= h(v) + 0x9e3779b9 + (s << 6) + (s >> 2);
 	}
