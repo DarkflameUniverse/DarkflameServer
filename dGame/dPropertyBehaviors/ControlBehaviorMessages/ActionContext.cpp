688babddbf8fbda59c2f4a10bf12a6c68d90b2ef
jadebenn <jonahbenn@yahoo.com>
2024-02-11 14:42:35 -0600

    remove redundant std::string constructor calls

--- dGame/dPropertyBehaviors/ControlBehaviorMessages/ActionContext.cpp
+++ dGame/dPropertyBehaviors/ControlBehaviorMessages/ActionContext.cpp
@@ -10,15 +10,15 @@ ActionContext::ActionContext(const AMFArrayValue* arguments, const std::string& 
 }
 
 BehaviorState ActionContext::GetBehaviorStateFromArgument(const AMFArrayValue* arguments, const std::string& key) const {
-	const auto* const stateIDValue = arguments->Get<double>(std::string{ key });
-	if (!stateIDValue) throw std::invalid_argument("Unable to find behavior state from argument \"" + std::string{ key } + "\"");
+	const auto* const stateIDValue = arguments->Get<double>(key);
+	if (!stateIDValue) throw std::invalid_argument("Unable to find behavior state from argument \"" + key + "\"");
 
 	return static_cast<BehaviorState>(stateIDValue->GetValue());
 }
 
 StripId ActionContext::GetStripIdFromArgument(const AMFArrayValue* arguments, const std::string& key) const {
-	const auto* const stripIdValue = arguments->Get<double>(std::string{ key });
-	if (!stripIdValue) throw std::invalid_argument("Unable to find strip ID from argument \"" + std::string{ key } + "\"");
+	const auto* const stripIdValue = arguments->Get<double>(key);
+	if (!stripIdValue) throw std::invalid_argument("Unable to find strip ID from argument \"" + key + "\"");
 
 	return static_cast<StripId>(stripIdValue->GetValue());
 }
