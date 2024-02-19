cee38f56648c6af2b34b3183c6485198babfbb11
jadebenn <jadebenn@users.noreply.github.com>
2024-02-19 07:23:34 -0600

    Update BlockDefinition.h - remove extraneous semicolons

--- dGame/dPropertyBehaviors/BlockDefinition.h
+++ dGame/dPropertyBehaviors/BlockDefinition.h
@@ -3,6 +3,7 @@
 
 #include <string>
 
+
 class AMFArrayValue;
 
 class BlockDefinition {
@@ -10,12 +11,12 @@ public:
 	BlockDefinition(const std::string_view defaultValue = "", const float minimumValue = 0.0f, const float maximumValue = 0.0f);
 	static BlockDefinition blockDefinitionDefault;
 
-	[[nodiscard]] std::string_view GetDefaultValue() const { return m_DefaultValue; };
-	[[nodiscard]] float GetMinimumValue() const noexcept { return m_MinimumValue; };
-	[[nodiscard]] float GetMaximumValue() const noexcept { return m_MaximumValue; };
-	void SetDefaultValue(const std::string_view value) { m_DefaultValue = std::string{ value }; };
-	void SetMinimumValue(const float value) noexcept { m_MinimumValue = value; };
-	void SetMaximumValue(const float value) noexcept { m_MaximumValue = value; };
+	[[nodiscard]] std::string_view GetDefaultValue() const { return m_DefaultValue; }
+	[[nodiscard]] float GetMinimumValue() const noexcept { return m_MinimumValue; }
+	[[nodiscard]] float GetMaximumValue() const noexcept { return m_MaximumValue; }
+	void SetDefaultValue(const std::string_view value) { m_DefaultValue = std::string{ value }; }
+	void SetMinimumValue(const float value) noexcept { m_MinimumValue = value; }
+	void SetMaximumValue(const float value) noexcept { m_MaximumValue = value; }
 
 private:
 	std::string m_DefaultValue;
