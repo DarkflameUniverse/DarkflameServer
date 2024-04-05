06e7d57e0d9c9929175956a03c2b16b12162e873
jadebenn <jadebenn@users.noreply.github.com>
2024-04-05 00:52:26 -0500

    chore: Remove dpEntity pointers from collision checking (#1529)
    * chore: Remove dpEntity pointers from collision checking
    
    * Update fn documentation in ProximityMonitorComponent.h
    
    * use more idiomatic method to calculate vector index
    
    * feedback
    
    * missed a ranges::find replacement
    
    * adjust for feedback. last changes tonight.
    
    * okay, also remove unneeded include. then sleep.
    
    * for real tho
    
    * update to use unordered_set instead of set

--- dGame/dComponents/BaseCombatAIComponent.cpp
+++ dGame/dComponents/BaseCombatAIComponent.cpp
@@ -150,13 +150,13 @@ void BaseCombatAIComponent::Update(const float deltaTime) {
 	m_dpEntityEnemy->SetPosition(m_Parent->GetPosition());
 
 	//Process enter events
-	for (auto en : m_dpEntity->GetNewObjects()) {
-		m_Parent->OnCollisionPhantom(en->GetObjectID());
+	for (const auto id : m_dpEntity->GetNewObjects()) {
+		m_Parent->OnCollisionPhantom(id);
 	}
 
 	//Process exit events
-	for (auto en : m_dpEntity->GetRemovedObjects()) {
-		m_Parent->OnCollisionLeavePhantom(en->GetObjectID());
+	for (const auto id : m_dpEntity->GetRemovedObjects()) {
+		m_Parent->OnCollisionLeavePhantom(id);
 	}
 
 	// Check if we should stop the tether effect
