5049f215bad44f3e5bfdf0e77c469d7fc75cf334
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2024-04-13 21:41:51 -0700

    chore: Use string to access SQLite columns (#1535)
    * use string to access field name
    
    * Update DEVGMCommands.cpp
    
    * corrected column name
    
    * constexpr array
    
    include <array>
    
    Revert "constexpr array"
    
    This reverts commit 1492e8b1773ed5fbbe767c74466ca263178ecdd4.
    
    Revert "include <array>"
    
    This reverts commit 2b7a67e89ad673d420f496be97f9bc51fd2d5e59.
    
    include <array>
    
    constexpr array
    
    ---------
    
    Co-authored-by: jadebenn <jonahbenn@yahoo.com>

--- dGame/dComponents/InventoryComponent.cpp
+++ dGame/dComponents/InventoryComponent.cpp
@@ -1094,7 +1094,7 @@ void InventoryComponent::CheckItemSet(const LOT lot) {
 	auto result = query.execQuery();
 
 	while (!result.eof()) {
-		const auto id = result.getIntField(0);
+		const auto id = result.getIntField("setID");
 
 		bool found = false;
 
