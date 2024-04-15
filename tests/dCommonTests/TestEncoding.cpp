fafe2aefadc9844429f66e6dc20478e41a419f14
jadebenn <jadebenn@users.noreply.github.com>
2024-04-15 01:14:54 -0500

    chore: Nitpicking-utils (#1549)
    * nit
    
    * GeneralUtils const-correctness and minor fixes
    
    * use copy instead of reference for char iteration loops
    
    * fix typo and reorganize some functions

--- tests/dCommonTests/TestEncoding.cpp
+++ tests/dCommonTests/TestEncoding.cpp
@@ -15,12 +15,12 @@ TEST_F(EncodingTest, TestEncodingHello) {
 	originalWord = "Hello World!";
 	originalWordSv = originalWord;
 
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'H');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'e');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'l');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'l');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'o');
-	EXPECT_EQ(GeneralUtils::_NextUTF8Char(originalWordSv, out), true);
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'H');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'e');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'l');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'l');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 'o');
+	EXPECT_EQ(GeneralUtils::details::_NextUTF8Char(originalWordSv, out), true);
 
 	EXPECT_EQ(GeneralUtils::UTF8ToUTF16("Hello World!"), u"Hello World!");
 };
@@ -29,15 +29,15 @@ TEST_F(EncodingTest, TestEncodingUmlaut) {
 	originalWord = reinterpret_cast<const char*>(u8"Frühling");
 	originalWordSv = originalWord;
 
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'F');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'r');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'ü');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'h');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'l');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'i');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'n');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'g');
-	EXPECT_EQ(GeneralUtils::_NextUTF8Char(originalWordSv, out), false);
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'F');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'r');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'ü');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'h');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'l');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'i');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'n');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'g');
+	EXPECT_EQ(GeneralUtils::details::_NextUTF8Char(originalWordSv, out), false);
 
 	EXPECT_EQ(GeneralUtils::UTF8ToUTF16("Frühling"), u"Frühling");
 };
@@ -46,10 +46,10 @@ TEST_F(EncodingTest, TestEncodingChinese) {
 	originalWord = "中文字";
 	originalWordSv = originalWord;
 
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'中');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'文');
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'字');
-	EXPECT_EQ(GeneralUtils::_NextUTF8Char(originalWordSv, out), false);
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'中');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'文');
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, U'字');
+	EXPECT_EQ(GeneralUtils::details::_NextUTF8Char(originalWordSv, out), false);
 
 	EXPECT_EQ(GeneralUtils::UTF8ToUTF16("中文字"), u"中文字");
 };
@@ -58,11 +58,11 @@ TEST_F(EncodingTest, TestEncodingEmoji) {
 	originalWord = "👨‍⚖️";
 	originalWordSv = originalWord;
 
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 0x1F468);
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 0x200D);
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 0x2696);
-	GeneralUtils::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 0xFE0F);
-	EXPECT_EQ(GeneralUtils::_NextUTF8Char(originalWordSv, out), false);
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 0x1F468);
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 0x200D);
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 0x2696);
+	GeneralUtils::details::_NextUTF8Char(originalWordSv, out); EXPECT_EQ(out, 0xFE0F);
+	EXPECT_EQ(GeneralUtils::details::_NextUTF8Char(originalWordSv, out), false);
 
 	EXPECT_EQ(GeneralUtils::UTF8ToUTF16("👨‍⚖️"), u"👨‍⚖️");
 };
