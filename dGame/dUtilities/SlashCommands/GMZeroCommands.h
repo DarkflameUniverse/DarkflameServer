784513164945d4021e80193129f988ab1929a44a
wincent <wincent.holm@gmail.com>
2024-05-24 15:06:40 +0200

    Test for reactive item descriptions

--- dGame/dUtilities/SlashCommands/GMZeroCommands.h
+++ dGame/dUtilities/SlashCommands/GMZeroCommands.h
@@ -15,6 +15,7 @@ namespace GMZeroCommands {
 	void LeaveZone(Entity* entity, const SystemAddress& sysAddr, const std::string args);
 	void Resurrect(Entity* entity, const SystemAddress& sysAddr, const std::string args);
 	void InstanceInfo(Entity* entity, const SystemAddress& sysAddr, const std::string args);
+	void ItemDescription(Entity* entity, const SystemAddress& sysAddr, const std::string args);
 	void ClientHandled(Entity* entity, const SystemAddress& sysAddr, const std::string args);
 }
 
