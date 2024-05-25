5667591635c65a51e44984ef0a362df468288b9b
dinomking33 <38479763+TAHuntling@users.noreply.github.com>
2024-05-25 01:25:56 -0500

    Testing Scripts
    Testing splitting AgSpaceStuff into AgSpaceStuff and AgShipShake

---
+++ dScripts/ai/AG/AgShipShake.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "CppScripts.h"
+
+class AgShipShake : public CppScripts::Script {
+public:
+	void OnStartup(Entity* self) override;
+	void OnTimerDone(Entity* self, std::string timerName) override;
+	void DoShake(Entity* self, bool explodeIdle);
+
+	std::string DebrisFX = "DebrisFX";
+	std::string ShipFX = "ShipFX";
+	std::string ShipFX2 = "ShipFX2";
+	std::u16string FXName = u"camshake-bridge";
+
+private:
+	Entity* GetEntityInGroup(const std::string& group);
+};
