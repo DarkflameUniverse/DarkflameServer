642d41b9f6914bb9d2acaa484f35ce196cf5d1e8
David Markowitz <EmosewaMC@gmail.com>
2024-06-12 14:47:05 -0700

    remove const ig?

--- dGame/dComponents/RacingControlComponent.cpp
+++ dGame/dComponents/RacingControlComponent.cpp
@@ -828,8 +828,8 @@ void RacingControlComponent::Update(float deltaTime) {
 
 			// Reached the start point, lapped
 			if (respawnIndex == 0) {
-				const auto now = std::chrono::high_resolution_clock::now();
-				const auto lapTime = std::chrono::duration_cast<std::chrono::milliseconds>(now - (player.lap == 0 ? m_StartTime : player.lapTime));
+				auto now = std::chrono::high_resolution_clock::now();
+				auto lapTime = std::chrono::duration_cast<std::chrono::milliseconds>(now - (player.lap == 0 ? m_StartTime : player.lapTime));
 
 				// Cheating check
 				if (lapTime.count() < 40000) {
