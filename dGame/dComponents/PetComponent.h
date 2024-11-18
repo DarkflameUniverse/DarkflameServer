d8b86072d40f0599dc2974303e752034e8a6bc5f
Tiernan <38479763+TAHuntling@users.noreply.github.com>
2024-11-17 20:48:48 -0600

    Fixed Misspelling of Tresure variable for petcomponent (#1617)

--- dGame/dComponents/PetComponent.h
+++ dGame/dComponents/PetComponent.h
@@ -329,7 +329,7 @@ private:
 	 * Timer that tracks how long a pet has been digging up some treasure, required to spawn the treasure contents
 	 * on time
 	 */
-	float m_TresureTime;
+	float m_TreasureTime;
 
 	/**
 	 * The position that this pet was spawned at
