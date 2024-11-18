cea66147f57ab7b30489318f4a490885738070fb
jadebenn <jadebenn@users.noreply.github.com>
2024-11-17 20:34:13 -0600

    fix: Cache compiler variables so external tools can recognize the compiler in use

--- cmake/toolchains/linux-gnu.cmake
+++ cmake/toolchains/linux-gnu.cmake
@@ -4,8 +4,8 @@ find_program(GNU_CXX_COMPILER c++ | g++ REQUIRED)
 
 # Debug messages
 message(DEBUG "GNU_C_COMPILER = ${GNU_C_COMPILER}")
-message(DEBUG "GNU_CXX_COMPILER = ${GNU_C_COMPILER}")
+message(DEBUG "GNU_CXX_COMPILER = ${GNU_CXX_COMPILER}")
 
-# Set compilers to clang
-set(CMAKE_C_COMPILER ${GNU_C_COMPILER})
-set(CMAKE_CXX_COMPILER ${GNU_CXX_COMPILER})
+# Set compilers to GNU (need to cache for VSCode tools to work correctly)
+set(CMAKE_C_COMPILER ${GNU_C_COMPILER} CACHE STRING "Set C compiler")
+set(CMAKE_CXX_COMPILER ${GNU_CXX_COMPILER} CACHE STRING "Set C++ compiler")
