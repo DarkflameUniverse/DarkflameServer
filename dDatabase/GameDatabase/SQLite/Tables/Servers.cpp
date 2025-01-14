f4b55915bc0c778b2256ac8f97a427605cd12db2
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2025-01-14 11:21:58 -0800

    fix: server runs now (#1730)

--- dDatabase/GameDatabase/SQLite/Tables/Servers.cpp
+++ dDatabase/GameDatabase/SQLite/Tables/Servers.cpp
@@ -4,7 +4,7 @@ void SQLiteDatabase::SetMasterInfo(const MasterInfo& info) {
 	// We only want our 1 entry anyways, so we can just delete all and reinsert the one we want
 	// since it would be two queries anyways.
 	ExecuteDelete("DELETE FROM servers;");
-	ExecuteInsert("INSERT INTO `servers` (`name`, `ip`, `port`, `state`, `version`, `master_password`) VALUES ('master', ?, ?, 0, 171022 ?)", info.ip, info.port, info.password);
+	ExecuteInsert("INSERT INTO `servers` (`name`, `ip`, `port`, `state`, `version`, `master_password`) VALUES ('master', ?, ?, 0, 171022, ?)", info.ip, info.port, info.password);
 }
 
 std::optional<IServers::MasterInfo> SQLiteDatabase::GetMasterInfo() {
