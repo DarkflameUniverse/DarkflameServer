49aa632d4224017ef3eb2b10628a401de4a7d470
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2025-07-12 22:21:45 -0700

    feat: WaypointReached notification for MovementAI and don't move the AI when its not built (#1844)

--- dGame/dComponents/MovementAIComponent.h
+++ dGame/dComponents/MovementAIComponent.h
@@ -209,6 +209,8 @@ public:
 	 */
 	static float GetBaseSpeed(LOT lot);
 
+	bool IsPaused() const { return m_Paused; }
+
 private:
 
 	/**
@@ -323,6 +325,9 @@ private:
 	NiPoint3 m_SavedVelocity;
 
 	bool m_IsBounced{};
+
+	// The number of waypoints that were on the path in the call to SetPath
+	uint32_t m_CurrentPathWaypointCount{ 0 };
 };
 
 #endif // MOVEMENTAICOMPONENT_H
