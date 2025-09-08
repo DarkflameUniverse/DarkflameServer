566a18df38c07ec4ce872930a552bf78cd2653d5
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2025-09-07 18:03:00 -0700

    Show git download progress with FetchContent (#1869)
    Some downloads are slower and showing progress is better than sitting there doing nothing

--- cmake/FindGoogleTest.cmake
+++ cmake/FindGoogleTest.cmake
@@ -6,6 +6,7 @@ FetchContent_Declare(
 	googletest
 	GIT_REPOSITORY https://github.com/google/googletest.git
 	GIT_TAG release-1.12.1
+	GIT_PROGRESS TRUE
 )
 
 # For Windows: Prevent overriding the parent project's compiler/linker settings
