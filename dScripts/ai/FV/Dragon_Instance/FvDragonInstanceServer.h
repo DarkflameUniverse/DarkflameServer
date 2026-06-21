5fa158298bc1f611e46cfaeb3b706a8c704958c4
David Markowitz <EmosewaMC@gmail.com>
2026-06-20 22:24:56 -0700

    Update FvDragonInstanceServer.h

--- dScripts/ai/FV/Dragon_Instance/FvDragonInstanceServer.h
+++ dScripts/ai/FV/Dragon_Instance/FvDragonInstanceServer.h
@@ -1,8 +1,7 @@
 #pragma once
 #include "CppScripts.h"
 
-class FvDragonInstanceServer : public CppScripts::Script
-{
+class FvDragonInstanceServer : public CppScripts::Script {
 public:
 	void OnPlayerLoaded(Entity* self, Entity* player) override;
 };
