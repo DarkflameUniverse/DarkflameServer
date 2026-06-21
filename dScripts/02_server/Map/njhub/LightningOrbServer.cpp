8c30abb4bfb1bd3ea20e37bdacdacf3eca9382ef
David Markowitz <39972741+EmosewaMC@users.noreply.github.com>
2026-06-20 22:10:57 -0700

    Update LightningOrbServer.cpp

--- dScripts/02_server/Map/njhub/LightningOrbServer.cpp
+++ dScripts/02_server/Map/njhub/LightningOrbServer.cpp
@@ -6,7 +6,7 @@ void LightningOrbServer::OnCollisionPhantom(Entity* self, Entity* target) {
 	GameMessages::GetPosition selfPos;
 	selfPos.Send(self->GetObjectID());
 	const NiPoint3 newVec((playerPos.pos.x - selfPos.pos.x) * 2.5, 15, (playerPos.pos.z - selfPos.pos.z) * 2.5);
-	// moving platforms don't work lol.  disable this so people can actually do the puzzle
+	// ahhhh aron said to put a TODO here moving platforms don't work lol.  disable this so people can actually do the puzzle
 	// GameMessages::SendKnockback(target->GetObjectID(), self->GetObjectID(), self->GetObjectID(), 0, newVec);
 	// GameMessages::SendPlayFXEffect(target->GetObjectID(), -1, u"knockback", "knockback");
 }
